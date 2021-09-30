5977ac468729198dc490c67210af449e87fb13eb
amyfranses <amyfranses@gmail.com>
2021-09-30 17:33:11 +0000

    Initial commit with javascripting work so far

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,9 @@
+
+
+function math(x, y, z){
+return (y * z) + x;
+}
+
+let result = math(53, 61, 67);
+
+console.log(math(53, 61, 67));
---
+++ javascripting/if-statement.js
@@ -0,0 +1,6 @@
+let fruit = "banana";
+if (fruit > 5) {
+  console.log('The fruit name has less than five characters.');
+} else {
+  console.log('The fruit name has more than five characters.');
+}
